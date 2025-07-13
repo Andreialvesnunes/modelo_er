fafd20b04416fb60bc4fa5e502deb720427eee1d
Andrei Alves <andreialvescvr2005@gmail.com>
2025-07-13 10:54:06 -0300

    feat(cliente): adicionar método de cadastro por etapas
    Criei métodos separados para cadastrar nome e CPF, e outro para idade.
    Depois, uni todos em um método principal que chama os anteriores.

--- src/service/Cliente.java
+++ src/service/Cliente.java
@@ -1,9 +1,20 @@
 package service;
 
+import excecao.CpfDublicadoException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Cliente {
     private String nomeCliente;
     private int idade;
     private String cpf;
+    private String respostaUsuario;
+    private static final Map<String, String> cliente = new HashMap<>();
+
+    private final Scanner scanner = new Scanner(System.in);
+
 
     public String getNomeCliente() {
         return nomeCliente;
@@ -27,5 +38,33 @@ public class Cliente {
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+
+    public void cadastarCliente(){
+        cadastrarCpfENome();
+        cadastarIdade();
+    }
+
+    private void cadastrarCpfENome(){
+        System.out.println("Digite seu cpf: ");
+        this.cpf = scanner.next();
+        setCpf(this.cpf);
+
+        if (cliente.containsKey(cpf)){
+            throw new CpfDublicadoException(cpf);
+        }
+
+        scanner.nextLine();
+        System.out.println("Digite seu nome: ");
+        this.nomeCliente = scanner.nextLine();
+        setNomeCliente(nomeCliente);
+
+        cliente.putIfAbsent(this.cpf, this.nomeCliente);
+    }
+
+    private void cadastarIdade(){
+        System.out.println("Olá " + this.nomeCliente + " digite sua idade: ");
+        this.idade = scanner.nextInt();
+        setIdade(idade);
     }
 }
